fbf622ec2e15dd385e66df96bb03eb9e9c263bec
KimSeungkyun <dirokim7@gmail.com>
2024-04-11 18:08:16 +0900

    feat toss fix

--- src/main/java/com/payment/app/TestController.java
+++ src/main/java/com/payment/app/TestController.java
@@ -1,5 +1,8 @@
 package com.payment.app;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,6 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 	@RequestMapping("/")
 	public String test () throws Exception {
+		Map<String,Object> map =  new HashMap<String, Object>();
+		Map<String,Object> map2 = new HashMap<String, Object>();
+		map2.put("3",3);
+		map.put("1",1);
+		map.put("2", map2);
+		System.out.println(map);
+		System.out.println(map.get("2"));
 		return "index";
 	}
 }
